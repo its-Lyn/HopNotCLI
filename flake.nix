b4b3e67c34398a334dd0c3f5070b14e38403f483
Grimmauld <Grimmauld@grimmauld.de>
2024-11-26 23:39:44 +0100

    github actions check

--- flake.nix
+++ flake.nix
@@ -3,10 +3,19 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      nix-github-actions,
+    }:
     let
       systems = [ "x86_64-linux" ];
       legacy = nixpkgs.legacyPackages;
@@ -16,5 +25,9 @@
         default = legacy.${system}.callPackage ./nix/package.nix { };
         hopnot = self.packages.${system}.default;
       });
+
+      githubActions = nix-github-actions.lib.mkGithubMatrix {
+        checks = nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.packages; # only check compiling, so passing packages is fine
+      };
     };
 }
