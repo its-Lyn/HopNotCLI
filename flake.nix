acf55bf24bfa237f42f4ca63ec778331245f371d
Grimmauld <Grimmauld@grimmauld.de>
2024-11-26 23:34:14 +0100

    fix nixpkgs flake input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "HopNot build for nix!";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
